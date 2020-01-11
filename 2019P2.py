a8be7c596257995d71472c822eef5a10323f3424
Caleb Hyun <calebhyun@Dons-Mac-mini.local>
2020-01-11 13:27:32 -0800

    commented

--- 2019P2.py
+++ 2019P2.py
@@ -14,6 +14,9 @@ for i in range(0, intNum):
     strNumOfFriends = f.readline()
     intNumOfFriends = int(strNumOfFriends)
 
+
+    # create a variable for the highest number ranking
+    # start it at 0 for now.
     maxNum = 0
     #Loop through each of the friends
     for x in range(0, intNumOfFriends):
@@ -22,23 +25,34 @@ for i in range(0, intNum):
         strLine = f.readline()
         strLine = strLine.strip()  #remove the \n at the end
 
+        # split the string into friend and ranking
         AryFriend = strLine.split()
 
+        # the 2nd item in the array AryFriend[1] is the ranking.  
+        # convert it and store it in an int variable
         intIndex = int(AryFriend[1])
 
+        
+        # if the intIndex is more than maxNum, then reset maxNum to intIndex
+        # we are trying to find the largest number ranking
         if intIndex > maxNum:
             maxNum = intIndex
 
+        # store in the dictionary by ranking
         friends[str(intIndex)] = AryFriend[0]
 
+    # create a string variable that will be printed in the end.
     friendsList=""
-    for y in range(maxNum, 0, -1):
-        try:
-            friendsList += friends[str(y)] + ", "
-        except KeyError:
-            doNothing=True
-    
 
+    # loop from the largest ranking number to 1.
+    for y in range(maxNum, 0, -1):
+
+        # check to see if the ranking (key) exists in the dictionary
+        if str(y) in friends:
+            #if so, then add the friend to the friendsList string variable
+            friendsList += friends[str(y)] + ", "
+
+    # Where you're all done, remove the last two characters ", "  then print
     print(friendsList[:-2])
 
 
